43d0e6cbdc2a9371d96835c716b758881ad57e80
Gourav <gourav052003@gmail.com>
2025-08-25 21:05:03 +0530

    Added Embedding Models
    Signed-off-by: Gourav <gourav052003@gmail.com>

---
+++ EmbeddingModels/document_similarity.py
@@ -0,0 +1,27 @@
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
+load_dotenv()
+embedding = OpenAIEmbeddings(model='text-embedding-3-large', dimensions=300)
+
+documents = [
+    "Virat Kohli is an Indian cricketer known for his aggressive batting and leadership.",
+    "MS Dhoni is a former Indian captain famous for his calm demeanor and finishing skills.",
+    "Sachin Tendulkar, also known as the 'God of Cricket', holds many batting records.",
+    "Rohit Sharma is known for his elegant batting and record-breaking double centuries.",
+    "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
+]
+
+query = 'tell me about virt kholi'
+
+document_embeddings = embedding.embed_documents(documents)
+query_embedding = embedding.embed_query(query)
+
+similarities = cosine_similarity([query_embedding], document_embeddings)[0]
+index,score = sorted(list(enumerate(similarities)),key=lambda x:x[1])[-1]
+
+print(query)
+print(documents[index])
+print("similarity score:", score)
---
+++ EmbeddingModels/embedding_openai_docs.py
@@ -0,0 +1,18 @@
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+
+load_dotenv()
+
+embedding = OpenAIEmbeddings(model="text-embedding-3-large",dimensions=32)
+documents = [
+    "Delhi is the capital of India",
+    "Paris is the capital of France",  
+    "Tokyo is the capital of Japan",
+    "Canberra is the capital of Australia",
+    "Ottawa is the capital of Canada",
+]
+
+result = embedding.embed_documents(documents)
+
+print(result)
+
